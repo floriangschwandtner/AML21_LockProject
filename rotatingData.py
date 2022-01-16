f126fbf1e029e9b7fa87f94d7a7ddf80e70d7469
CreamyFoam <88207621+CreamyFoam@users.noreply.github.com>
2022-01-16 17:24:08 +0100

    debugs in for sentences and some modification

--- rotatingData.py
+++ rotatingData.py
@@ -56,15 +56,21 @@ def T3(x, phi):
     return r @ x
 
 #print(np.random.randint(1,4,(30,25)))
-
+a = np.array([1,0,0])
+print(T1(a,30))
+print(T2(a,30))
+print(T3(a,30))
+print(" Rotation is ok ")
 
 ################# get data
 data = np.load("LabeledOriginalMatrix.npy")
 
 #for test
-head=-1
-stack=25
-data = data[:head,:,:stack]
+#head=-1
+#stack=26
+#data = data[:head,:,:stack]
+
+
 #labels=np.random.randint(1,5,(30))
 #print(labels)
 #data[:,-1,:]=np.random.randint(1,5,(head,stack)) ###making labels for test
@@ -95,11 +101,19 @@ print(dataNew1.shape)
 print(dataNew2.shape)'''
 dataNew=data
 
+print("datashape",dataNew.shape)
+
 ###########define the range of each rotation
 #randomized values will be chosen from homogenous distribution (value) is the length of the matrix
-roll = np.random.randint(-10,10,(10))
-pitch= np.random.randint(-10,10,(10))
-yaw  = np.random.randint(-60,60,(20))
+roll = np.random.randint(1,10,(5))
+roll = np.append(roll,-1*roll)
+print("roll",roll)
+pitch= np.random.randint(1,10,(5))
+pitch = np.append(pitch,-1*pitch)
+print("pitch",pitch)
+yaw  = np.random.randint(1,60,(10))
+yaw = np.append(yaw,-1*yaw)
+print("yaw",yaw)
 
 ############creating the dataset
 
@@ -107,28 +121,66 @@ x=dataNew.shape[0]
 y=dataNew.shape[1]
 z=dataNew.shape[2]
 
+ShowNewData = False
+
 #Roll
+print("Roll")
 DataRoll=np.zeros((x,y,z*roll.shape[0]))
 for k in range(roll.shape[0]):
-    for j in range(y):
+    #print("phi", roll[k])
+    for j in range(z):
+        #print("j", j)
         for i in range(x):
+            #print("i",i)
             DataRoll[i,:-1,k*z+j]=T1(dataNew[i,:-1,j],roll[k])
+            #print(DataRoll[i,:-1,k*z+j])
         DataRoll[:,-1,k*z+j]=dataNew[:,-1,j]
+
+
+
+if ShowNewData ==True:
+    fig1 = plt.figure(figsize=(8,10))
+    for i in range(DataRoll.shape[2]):
+        ax = fig1.add_subplot(DataRoll.shape[2],1,i+1)
+        ax.plot(DataRoll[:,0,i],DataRoll[:,1,i], '*b')
+
+plt.show()
+
+
+
 #Pitch
+print("Pitch")
 DataPitch=np.zeros((x,y,z*pitch.shape[0]))
 for k in range(pitch.shape[0]):
-    for j in range(y):
+    for j in range(z):
         for i in range(x):
             DataPitch[i,:-1,k*z+j]=T2(dataNew[i,:-1,j],pitch[k])
         DataPitch[:,-1,k*z+j]=dataNew[:,-1,j]
 
+if ShowNewData ==True:
+    fig2 = plt.figure(figsize=(8,10))
+    for i in range(DataPitch.shape[2]):
+        ax = fig2.add_subplot(DataPitch.shape[2],1,i+1)
+        ax.plot(DataPitch[:,0,i],DataPitch[:,1,i], '*b')
+
+plt.show()
+
+
 #Yaw
+print("Yaw")
 DataYaw=np.zeros((x,y,z*yaw.shape[0]))
-for k in range(pitch.shape[0]):
-    for j in range(y):
+for k in range(yaw.shape[0]):
+    for j in range(z):
         for i in range(x):
             DataYaw[i,:-1,k*z+j]=T2(dataNew[i,:-1,j],yaw[k])
         DataYaw[:,-1,k*z+j]=dataNew[:,-1,j]
+
+if ShowNewData ==True:
+    fig3 = plt.figure(figsize=(8,10))
+    for i in range(DataYaw.shape[2]):
+        ax = fig3.add_subplot(DataYaw.shape[2],1,i+1)
+        ax.plot(DataYaw[:,0,i],DataYaw[:,1,i], '*b')
+plt.show()
 
 print(dataNew.shape)
 print(DataRoll.shape)
